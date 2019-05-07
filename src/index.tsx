28cbca4bb77ac8d9b4f563504bbe451b73e08251
jmargielewski <kuba.margielewski@gmail.com>
2019-05-07 20:58:17 +0100

    add typescript and eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Grow Raw</h1>, document.getElementById('root'));
+ReactDOM.render(<div>Grow Raw</div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
